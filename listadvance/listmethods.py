77c4cab6b47c644f4c582872015205445b21365b
Mayun-Binod <shresthabinod334@gmail.com>
2026-09-13 20:49:36 +0545

    list loop and indexing

---
+++ listadvance/listmethods.py
@@ -0,0 +1,67 @@
+# # insert method
+# thislist = ["apple", "banana", "cherry",{"name":"sandhya oli"}]
+# thislist.insert(3,"mango")
+# print(thislist)
+
+# # append method
+# thislist = ["apple", "banana", "cherry"]
+# thislist.append("orange")
+# print(thislist)
+
+# thislist1=[]
+# thislist1.append("orange")
+# print(thislist1)
+
+# extend method
+# thislist = ["apple", "banana", "cherry"]
+# tropical = ["mango", "pineapple", "papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+# extend another way
+# thislist = ["apple", "banana", "cherry"]
+# thistuple = ("kiwi", "orange")
+# thislist.extend(thistuple)
+# print(thislist)
+
+# remove method
+# thislist = ["apple", "banana", "cherry"]
+# thislist.remove("banana")
+# print(thislist)
+
+# popmethod
+# thislist = ["apple", "banana", "cherry"]
+# thislist.pop(1)
+# print(thislist)
+
+# delete by indexing
+# thislist = ["apple", "banana", "cherry","mango"]
+# del thislist[0:2]
+# print(thislist)
+
+# clear method
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+# loop by length
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#   print(thislist[i])
+
+# for i in range(3):
+#   print(i)
+
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+# while i < len(thislist):
+#   print(thislist[i])
+#   i = i + 1
+
+
+# sort method /ascending order
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.reverse()
+print(thislist)
+
+  
